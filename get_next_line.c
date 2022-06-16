d53d5e4696e7887773f8f0f41e2511343a782e77
astahjmo <contato.johnatan06@gmail.com>
2022-06-16 16:31:16 -0300

    deus nos abandonou

--- get_next_line.c
+++ get_next_line.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: johmatos < johmatos@student.42sp.org.br    +#+  +:+       +#+        */
+/*   By: johmatos < johmatos@student.42sp.org.br    +#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaarbe+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 21:19:24 by johmatos          #+#    #+#             */
-/*   Updated: 2022/06/15 20:11:44 by astaroth         ###   ########.fr       */
+/*   Updated: 2022/06/16 16:28:05 by astaroth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
 #include <unistd.h>
 
 char	*get_line(char *buffer)
@@ -19,16 +20,25 @@ char	*get_line(char *buffer)
 	size_t	count;
 	size_t	cpy;
 
-	cpy = -1;
+	cpy = 0;
 	count = 0;
-	while (buffer[count] != '\n' && buffer[count])
+	if (!buffer[count])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	while (buffer[count] && buffer[count] != '\n')
 		count++;
 	line = ft_alloc(count + 2, sizeof(char));
-	while (++cpy <= count)
+	if (!line)
+		return (NULL);
+	while (cpy <= count)
+	{
 		line[cpy] = buffer[cpy];
+		cpy++;
+	}
 	line[cpy] = '\0';
 	cpy = 0;
-	count++;
 	while (buffer[count])
 	{
 		buffer[cpy] = buffer[count];
@@ -41,21 +51,27 @@ char	*get_line(char *buffer)
 
 char	*ft_handler_buffer(char *buffer, int fd)
 {
-	char	read_buff[BUFFER_SIZE + 1];
+	char	*read_buff;
 	int		status;
 	char	*temp;
 
 	status = BUFFER_SIZE;
-	while (!ft_strchr(buffer, '\n') && status == BUFFER_SIZE)
+	read_buff = ft_alloc(BUFFER_SIZE + 1, sizeof(char));
+	while (!ft_strchr(buffer, '\n') && status != 0)
 	{
 		status = read(fd, read_buff, BUFFER_SIZE);
-		read_buff[status] = '\0';
+		read_buff[BUFFER_SIZE] = '\0';
 		if (status < 0)
+		{
+			free (buffer);
+			free(read_buff);
 			return (NULL);
+		}
 		temp = ft_new_buffer(buffer, read_buff);
 		free (buffer);
 		buffer = temp;
 	}
+	free (read_buff);
 	return (buffer);
 }
 
@@ -69,11 +85,8 @@ char	*get_next_line(int fd)
 		return (NULL);
 	if (!buffer)
 		buffer = ft_alloc(BUFFER_SIZE, sizeof(char));
-	if ((buffer != NULL || !ft_strchr(buffer, '\n')))
-	{
-		buffer = ft_handler_buffer(buffer, fd);
-	}
-	if (!ft_strlen(buffer))
+	buffer = ft_handler_buffer(buffer, fd);
+	if (!buffer)
 		return (line);
 	line = get_line(buffer);
 	return (line);
